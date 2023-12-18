cced232b4857132f31c62666638386525fac6655
Aidana Kalakova <aidana@Aidanas-MacBook-Air.local>
2023-12-19 00:31:22 +0600

    feat: solve sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function sortByHeight(arr) {
+  let temp = [];
+  for (let i in arr) {
+    if (arr[i] !== -1) {
+      temp.push(arr[i]);
+    }
+  }
+  temp.sort();
+  console.log("temp: ", temp);
+  let j = 0;
+  for (let i in arr) {
+    if (arr[i] !== -1) {
+      arr[i] = temp[j];
+      j++;
+    }
+  }
+  return arr;
 }
 
 module.exports = {
