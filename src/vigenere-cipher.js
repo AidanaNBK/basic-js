708d4399fce75594b749b4d68fd52c72603ea8a5
Aidana K <aidana.amnbk@gmail.com>
2024-12-18 22:50:32 +0500

    ft/correct solution for vigenere-cipher problem

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,42 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
+  }
   encrypt(text, key) {
     if (!text || !key) {
-      throw new Error("Incorrect arguments!");
+      throw new Error('Incorrect arguments!');
     }
-    let res = "";
-    let index = 0;
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
     for (let i = 0; i < text.length; i++) {
-      if (
-        (text[i] >= "A" && text[i] <= "Z") ||
-        (text[i] >= "a" && text[i] <= "z")
-      ) {
-        let shift = this.shiftValue(key[index]);
-        let encrypted = this.encryptVal(text[i], shift);
-        res += encrypted;
-        index = (index + 1) % key.length;
+      if (text[i] >= 'A' && text[i] <= 'Z') {
+        const shift = key[keyIndex % key.length].charCodeAt(0) - 'A'.charCodeAt(0);
+        const charCode = text[i].charCodeAt(0) - 'A'.charCodeAt(0);
+        const encryptedCharCode = (charCode + shift) % 26;
+        result += String.fromCharCode(encryptedCharCode + 'A'.charCodeAt(0));
+        keyIndex++;
       } else {
-        res += text[i];
+        result += text[i];
       }
     }
-    return res;
+    return this.direct ? result : result.split('').reverse().join('');
   }
   decrypt(text, key) {
     if (!text || !key) {
-      throw new Error("Incorrect arguments!");
+      throw new Error('Incorrect arguments!');
     }
-  }
-  shiftValue(elem) {
-    let base =
-      elem.toUpperCase() === elem ? "A".charCodeAt(0) : "a".charCodeAt(0);
-    return elem.charCodeAt(0) - base;
-  }
-  encryptVal(elem, shift) {
-    let base =
-      elem.toUpperCase() === elem ? "A".charCodeAt(0) : "a".charCodeAt(0);
-    let encrypted = ((elem.charCodeAt(0) - base + shift + 26) % 26) + base;
-    return String.fromCharCode(encrypted);
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] >= 'A' && text[i] <= 'Z') {
+        const shift = key[keyIndex % key.length].charCodeAt(0) - 'A'.charCodeAt(0);
+        const charCode = text[i].charCodeAt(0) - 'A'.charCodeAt(0);
+        const decryptedCharCode = (charCode - shift + 26) % 26;
+        result += String.fromCharCode(decryptedCharCode + 'A'.charCodeAt(0));
+        keyIndex++;
+      } else {
+        result += text[i];
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
